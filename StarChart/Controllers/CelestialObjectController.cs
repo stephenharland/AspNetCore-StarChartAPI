43e85027a16762b2eb1f047daf7b048204130e57
Stephen Harland <stephen.harland1@nhs.net>
2019-07-19 13:50:51 +0100

    Completed the module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -37,6 +37,7 @@ namespace StarChart.Controllers
             return Ok(celestialObjects);
         }
 
+        [HttpGet]
         public IActionResult GetAll()
         {
             var celestialObjects = _context.CelestialObjects.ToList();
