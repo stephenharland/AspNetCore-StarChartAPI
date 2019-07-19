de3d6935dc992161f9164ef7cf3d5f82523355bc
Stephen Harland <stephen.harland1@nhs.net>
2019-07-19 10:03:52 +0100

    Completed the module

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using StarChart.Data;
 
 namespace StarChart
 {
@@ -11,12 +13,13 @@ namespace StarChart
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("StarChart"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            
+            app.UseMvc();
         }
     }
 }
